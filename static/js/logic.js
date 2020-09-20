47f015261c7beb85736ece339e3ea8d198371426
Govind Patwal <67445971+Govind-Patwal@users.noreply.github.com>
2020-09-20 18:18:51 -0400

    some changes here

--- static/js/logic.js
+++ static/js/logic.js
@@ -109,18 +109,24 @@ attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap
     accessToken: API_KEY
 });
 
+// We create the dark view tile layer that will be an option for our map.
+let satelliteStreets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    accessToken: API_KEY
+});
+
 // Create a base layer that holds both maps.
 let baseMaps = {
-  Street: streets,
-  Dark: dark,
-  Light: light
+  "Street": streets,
+  "Satellite Streets": satelliteStreets
 };
 
 // Create the map object with center, zoom level and default layer.
 let map = L.map('mapid', {
-  center: [44.0, -80],
-  zoom: 2,
-  layers: [dark]
+  center: [43.7, -79.3],
+  zoom: 11,
+  layers: [satelliteStreets]
 })
 
 // Pass our map layers into our layers control and add the layers control to the map.
@@ -136,10 +142,10 @@ let myStyle = {
 // let airportData = "https://raw.githubusercontent.com/Govind-Patwal/Mapping_Earthquakes/master/majorAirports.json";
 
 // Accessing the Toronto Routes
-let torontoData = "https://raw.githubusercontent.com/Govind-Patwal/Mapping_Earthquakes/master/torontoRoutes.json";
+let torontoHoods = "https://raw.githubusercontent.com/Govind-Patwal/Mapping_Earthquakes/master/torontoNeighborhoods.json";
 
 // Grabbing our GeoJSON data.
-d3.json(torontoData).then(function(data) {
+d3.json(torontoHoods).then(function(data) {
   // Creating a GeoJSON layer with the retrieved data.
   L.geoJson(data).addTo(map);
 
